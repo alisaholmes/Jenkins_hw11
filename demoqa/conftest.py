61e0110066981b00201b13c42bd0a5c07082d858
Julia Isaacson <alisa.holmes@mail.ru>
2024-07-27 23:46:22 +0300

    added selenoid

---
+++ demoqa/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_management():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.timeout = 6.0
+    browser.config.window_height = 1366
+    browser.config.window_width = 1024
+
+    yield browser
+    browser.quit()
